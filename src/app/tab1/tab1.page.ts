d9a037b68bd71760fee5300a93f613d595d579c4
lucette2314 <lucette2314@hotmail.com>
2024-01-20 12:24:05 -0500

    backend working

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Itask } from '../interface/itask';
+import { TasksService } from '../services/tasks.service';
 
 @Component({
   selector: 'app-tab1',
@@ -10,5 +12,13 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
 export class Tab1Page {
-  constructor() {}
+  //property
+  tasks!: Itask[];
+
+  constructor(private tasksService: TasksService){
+    tasksService.getTasks().subscribe((results) => {
+      this.tasks = results;
+      console.log(this.tasks);
+    });
+    }
 }
